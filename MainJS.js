1d5dc8fb6bff2a21a6484086a93afbef8caf17ad
rainbow211793 <rainbow211793@gmail.com>
2024-11-17 17:26:11 +0000

    rainbow

---
+++ MainJS.js
@@ -0,0 +1,10 @@
+function add() { // New fucntion
+ Clicks++; // Adding +1 to the variable
+ document.getElementById('clickcount').innerHTML = Clicks
+}
+
+let Clicks = 0; // To reset the Clicks when the page is refreshed.
+
+let upgrade = 0 // To make sure that the 'potential' new system for clicking is reset when the page is reset.
+
+// Code by @Rlolcat on X & @rainbow211793 on Discord
